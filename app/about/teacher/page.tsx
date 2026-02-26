647505a418561bf72b3faa36f2c111551db78891
Jornexe <62523659+Jornexe@users.noreply.github.com>
2026-02-26 18:31:31 +0100

    new backend with spacetime
    lost changes in archived repo:
    gullbrygg/aiguidebook
    
    Co-Authored-By: taca <59736579+astrotaca@users.noreply.github.com>
    Co-Authored-By: Edvin <114284703+cwedvin@users.noreply.github.com>
    Co-Authored-By: Daniel Larsen <larsen97daniel@gmail.com>

---
+++ app/about/teacher/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Teacher(){
+    return (
+      <>
+        <div className="justify-center flex flex-col items-center">
+            {/* <form action={signup}> */}
+            <div className="flex justify-around gap-14 pt-5 break-all [&>*]:min-w-56 [&>*]:h-[55vh] [&>*]:py-2 [&>*]:border-2 [&>*]:rounded-md [&>*]:text-center">
+                <div className="">
+                    <div className="border-b-2 flex justify-center">
+                      <Link href={"/about/student"} className="bg-white text-black w-fit hover:bg-purple-400 transition-all duration-200 rounded-md p-2 mb-2">Students LENKE</Link></div>
+                    <p className="">WEEEWOOO</p>
+                </div>
+                <div>
+                    <div className="border-b-2 flex justify-center">
+                        <Link href={"/about/teacher"} className="bg-white text-black w-fit hover:bg-purple-400 transition-all duration-200 rounded-md p-2 mb-2">Teachers LENKE</Link></div>
+                    <p className="">IDK ANYMORE</p>
+                </div>
+                <div>
+                    <div className="border-b-2 flex justify-center">
+                      <Link href={"/about/university"} className="bg-white text-black w-fit hover:bg-purple-400 transition-all duration-200 rounded-md p-2 mb-2">Universitet LENKE</Link></div>
+                    <p className="">¯\_(ツ)_/¯ Skriv tekst her eller noe : TEST...TEST...TEST...TEST...TEST...TEST...TEST...</p>
+                </div>
+            </div>
+            <div className="w-full mt-5 min-h-[20vh] border-2 rounded-md">
+              weeewooo
+            </div>
+            {/* </form> */}
+        </div>
+      </>  
+    );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+export default function Dashboard() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the Guidebook Dashboard</h1>
+        <button className="self-auto">Testing</button> <p> Hello World</p>
+
+      </div>
+    </>
+  );
+}
